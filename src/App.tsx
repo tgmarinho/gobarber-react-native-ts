d4eef1fc55cbb5499454ee94ed97c9f5d59cd1aa
Thiago Marinho <tgmarinho@gmail.com>
2020-04-27 10:40:35 -0400

    GoBarber RN App - Estrutura e Padrões - Configurando navegação

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
-import { View, Text, StatusBar } from 'react-native';
+import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './routes';
 
 const App: React.FC = () => (
-  <>
+  <NavigationContainer>
     <StatusBar barStyle="light-content" backgroundColor="#312e38" />
     <View style={{ flex: 1, backgroundColor: '#312e38' }}>
-      <Text>GoBarber</Text>
+      <Routes />
     </View>
-  </>
+  </NavigationContainer>
 );
 
 export default App;
