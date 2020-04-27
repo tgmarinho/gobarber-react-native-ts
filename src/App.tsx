8bfd97654e184cc9026ebe35327a8e10a09ffc78
Thiago Marinho <tgmarinho@gmail.com>
2020-04-27 10:12:46 -0400

    GoBarber RN App - Estrutura e Padrões - Padrões e estilo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, Text } from 'react-native';
 
-const App: React.FC = () => <View><Text>GoBarber</Text></View>;
+const App: React.FC = () => (
+  <View>
+    <Text>GoBarber</Text>
+  </View>
+);
 
 export default App;
