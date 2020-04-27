0507b1e7a7b851be605b2844f69fb52ac02f8f52
Thiago Marinho <tgmarinho@gmail.com>
2020-04-27 10:16:52 -0400

    GoBarber RN App - Estrutura e Padrões - Configurando StatusBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StatusBar } from 'react-native';
 
 const App: React.FC = () => (
-  <View>
-    <Text>GoBarber</Text>
-  </View>
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+      <Text>GoBarber</Text>
+    </View>
+  </>
 );
 
 export default App;
